e3b1d21bf16fc0b9b23c4f4731f2f3ea095e1c60
Jeremy Durden <jeremydurden@gmail.com>
2021-08-10 10:55:16 -0700

    adding logic for editing notes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 class App {
     constructor(){
         this.notes = [];
+        this.title = ''
+        this.text = ''
+        this.id = ''
 
 
         this.$form = document.querySelector('#form')
@@ -11,13 +14,17 @@ class App {
         this.$formCloseButton = document.querySelector('#form-close-button')
         this.$placeHolder = document.querySelector('#placeholder')
         this.$modal = document.querySelector(".modal")
+        this.$modalTitle = document.querySelector(".modal-title")
+        this.$modalText = document.querySelector(".modal-text")
+        this.$modalCloseButton = document.querySelector('.modal-close-button')
         this.addEventListeners();
     }
 
     addEventListeners(){
         document.body.addEventListener('click', event =>{
-        this.handleFormClick(event) 
-        this.openModal(event)
+        this.handleFormClick(event);
+        this.selectNote(event);
+        this.openModal(event);
         });
 
         this.$form.addEventListener('submit', event => {
@@ -35,6 +42,11 @@ class App {
             // on the entire form, not just the text input.
             this.closeForm()
         })
+
+        this.$modalCloseButton.addEventListener('click', event => {
+            this.closeModal(event)
+        })
+
     }
 
     handleFormClick(event){
@@ -75,7 +87,14 @@ class App {
         if (event.target.closest('.note')){
             console.log(this.$modal)
             this.$modal.classList.toggle('open-modal')
+            this.$modalTitle.value = this.title;
+            this.$modalText.value = this.text;
         }
+    }
+
+    closeModal(event){
+        this.editNote()
+        this.$modal.classList.toggle('open-modal')
     }
 
     addNote({ title, text }){
@@ -92,12 +111,30 @@ class App {
 
     }
 
+    editNote() {
+     const title = this.$modalTitle.value;
+     const text = this.$modalText.value;
+     this.notes = this.notes.map(note => 
+       note.id === Number(this.id) ? { ...note, title, text } : note
+     );
+     this.displayNotes();
+  }
+
+    selectNote(event){
+        const $selectedNote = event.target.closest('.note')
+        if(!$selectedNote) return;
+        const [$noteTitle, $noteText] = $selectedNote.children
+        this.title = $noteTitle.innerText;
+        this.text = $noteText.innerText;
+        this.id = $selectedNote.dataset.id;
+    }
+
     displayNotes(){
         const hasNotes = this.notes.length > 0;
         this.$placeHolder.style.display = hasNotes ?  'none' : 'flex'
 
         this.$notes.innerHTML = this.notes.map(note => `
-        <div style="background: ${note.color};" class="note">
+        <div style="background: ${note.color};" class="note" data-id="${note.id}">
           <div class="${note.title && 'note-title'}">${note.title}</div>
           <div class="note-text">${note.text}</div>
           <div class="toolbar-container">
